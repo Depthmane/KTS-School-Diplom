34eeece0cc1f9ad0afde57cd1fbc8fffaf360bd1
Depthmane <117308187+Depthmane@users.noreply.github.com>
2025-03-26 23:20:38 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import tsconfig from './tsconfig.json';
+import tsconfig from './tsconfig.app.json';
 
 const SRC_PATH = path.resolve(__dirname, 'src');
 
