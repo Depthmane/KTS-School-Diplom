0f205622913694d760a94aac0ae27f9ff56b925d
Depthmane <depthmane@yandex.ru>
2025-04-22 05:12:53 +0300

    - pre deploy

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -2,7 +2,7 @@ import authStore from "./AuthStore";
 import bandsStore from "./BandsStore";
 import favoriteBandsStore from "./FavoriteBandsStore";
 import filtersStore from "./FiltersStore";
-import {uiStore} from "./UIStore.ts";
+import {uiStore} from "./UIStore";
 import userStore from "./UserStore";
 
 export { bandsStore, uiStore, filtersStore,  authStore, favoriteBandsStore, userStore };
